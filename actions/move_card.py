8e0f4907bd4b07c59926608f3e63a5717b451e6b
nik <nik@nik.nik>
2018-09-19 16:18:36 +0300

    Add possibility to change board in move_card action

--- actions/move_card.py
+++ actions/move_card.py
@@ -2,11 +2,15 @@ from lib import action
 
 
 class MoveCardAction(action.BaseAction):
-    def run(self, card_id, target_list_id, api_key=None, token=None):
+    def run(self, card_id, target_list_id, target_board_id=None, api_key=None, token=None):
         if api_key:
             self._set_creds(api_key=api_key, token=token)
 
         card = self._client().get_card(card_id)
-        card.change_list(list_id=target_list_id)
+
+        if not target_board_id:
+            card.change_list(list_id=target_list_id)
+        else:
+            card.change_board(list_id=target_list_id, board_id=target_board_id)
 
         return card
